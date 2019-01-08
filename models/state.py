eaa909db9056b113bba9475fce3bde2d31eab4e3
Jenn <jennogden95@gmail.com>
2019-01-08 16:50:47 +0000

    ADD: Updated name attribute and added __tablename__ and cities attributes

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 
 class State(BaseModel):
@@ -8,4 +10,19 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column("name",
+                  String(128),
+                  nullable=False)
+    cities = relationship("City", backref=backref(
+                          "state", cascade="all, delete"))
+
+    def cities(self):
+        """ wut do this do? it tassteses like a hot plate
+        """
+        for k,v in something.item():
+            if alwjkeawe
+                then?
+            if this.stateid is the state it then
+        reurn the city with state id at ?
+ 
