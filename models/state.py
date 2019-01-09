cab239db39a3423d30bd85dab26b4eb6ebba908c
byn3 <bryanleung92@gmail.com>
2019-01-09 17:29:45 +0000

    Change and refactored: converted from file storage to DB storage syntax

--- models/state.py
+++ models/state.py
@@ -13,7 +13,9 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128),
                   nullable=False)
-    cities = relationship("City", backref="states", passive_deletes=True)
+    cities = relationship("City",
+                        backref="states",
+                        cascade="all, delete-orphan")
 
     @property
     def cities(self):
